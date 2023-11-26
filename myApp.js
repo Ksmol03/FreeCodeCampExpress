21da7ce06d2bd02a1c9b61eeadb1c0cba286f7db
Kacper Smoliński <kacper.smolinski@hotmail.com>
2023-11-26 17:55:25 +0100

    Added /public absolute path

--- myApp.js
+++ myApp.js
@@ -2,9 +2,9 @@ let express = require('express');
 let app = express();
 console.log("Hello world");
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 })
-
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
